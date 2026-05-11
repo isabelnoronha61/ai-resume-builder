437b031e1891b12af275fc140088d5bcb5fcca8b
isabel_Noronha <isabel_Noronha@dell.com>
2026-05-11 09:33:01 +0530

    upload pdf and extract text

--- app/routes/resume.py
+++ app/routes/resume.py
@@ -1,9 +1,15 @@
 from fastapi import APIRouter, UploadFile, File
+from app.services.parser import extract_text_from_pdf
 
 router = APIRouter()
 
 @router.post("/upload-resume")
 async def upload_resume(file: UploadFile = File(...)):
-    content =  file.read()
+    content =  await file.read()
 
-    return { "filename": file.filename, "size": len(content) }
+    extracted_text = extract_text_from_pdf(content)
+
+    return {
+        "filename": file.filename,
+        "text_preview": extracted_text[:500]  
+    }
